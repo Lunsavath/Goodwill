e34a2d45664cafd657b713a8a19752886a72d58b
Lunsavath <anush.nearbuzz.com>
2020-07-25 10:54:57 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Layout from "./Components/Layout/Layout";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Layout />
-      </div>
-    </Router>
+    <div>
+      <Layout />
+    </div>
   );
 }
 
